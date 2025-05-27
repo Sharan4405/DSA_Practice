0ea747e20967a026bbd292a5ea7c0a340e0f4ef2
Sharan4405 <lakshettysharanu@gmail.com>
2025-05-27 05:59:01 +0530

    daily practice

--- BinarySearch/Upper_Bound.cpp
+++ BinarySearch/Upper_Bound.cpp
@@ -1,31 +1,28 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int upperBound(vector<int> &arr, int x, int n) {
-    int low = 0, high = n - 1;
-    int ans = n;
+int upperBound(const vector<int> &arr, int x) {
+    size_t low = 0, high = arr.size();
+    size_t ans = arr.size();
 
-    while (low <= high) {
-        int mid = (low + high) / 2;
-        // maybe an answer
+    while (low < high) {
+        size_t mid = low + (high - low) / 2;
         if (arr[mid] > x) {
             ans = mid;
-            //look for smaller index on the left
-            high = mid - 1;
-        }
-        else {
-            low = mid + 1; // look on the right
+            high = mid;
+        } else {
+            low = mid + 1;
         }
     }
-    return ans;
+    return static_cast<int>(ans);
 }
 
 int main()
 {
     vector<int> arr = {3, 5, 8, 9, 15, 19};
-    int n = 6, x = 9;
-    int ind = upperBound(arr, x, n);
+    int x = 9;
+    int ind = upperBound(arr, x);
     cout << "The upper bound is the index: " << ind << "\n";
     return 0;
 }
-
